aff4123a5c0bb8b3172ff40fe521ccdf7b4d1728
AlHiraHud <jimy321@yahoo.com>
2025-11-03 21:32:03 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -16,3 +16,12 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+});
